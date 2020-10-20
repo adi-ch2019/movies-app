5677901c5a5dddff0d46e4f505ad29a55ca40ba8
adich <adityanaidu.c@tataelxsi.co.in>
2020-10-20 20:47:14 +0530

    test

--- src/Movies-Backend/adich.dev.movies.api/Startup.cs
+++ src/Movies-Backend/adich.dev.movies.api/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging; 
 
 namespace adich.dev.movies.api
 {
